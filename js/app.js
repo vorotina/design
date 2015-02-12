15db9356cff3e1ab7ce64d936c495d9701ebaa9c
unknown <katerynavo@PTUA-LS0036.EE.playtech.corp>
2015-02-12 23:12:53 +0200

    add libs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 require(
-    ['mymodule', 'jquery'],
-    function( Module, $ ){ //Module при этом не будет доступна в глобальной области видимости, доллар в глобальную область 
+    ['mymodule', 'jquery', '../../node_modules/underscore/underscore', '../../node_modules/backbone/backbone'],
+    function( Module, $, _, backbone ){ //Module при этом не будет доступна в глобальной области видимости, доллар в глобальную область 
+		var data = [{
+			
+		
+		
+		}];
+		
+		var Item = Backbone.Model.extend({
+			title : null,
+			picture : null,
+			descr : null
+		})
+		
+		var ItemCollection = Backbone.Collection.extend({
+			model : Item
+		})
+		
+		var Router = Backbone.Router.extend({
+			routes: {
+				"home": "home"
+			},
+			
+			home : function() {
+			  console.log("yolo");
+			}
+		})
+		
+		
+		new Router();
+		
+		Backbone.history.start({pushState: true});
+		
 		for (i=0; i<20; i++) {
 			$('.grid').append( Module.gallery );
 		}
